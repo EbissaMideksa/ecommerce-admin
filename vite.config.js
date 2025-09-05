68ec20459c75f42a93bd5fe55ba27a5239addb3f
EebbaaM <neweebbiwaan1@gmail.com>
2025-09-05 22:09:36 +0300

    Restore full project files and dependencies

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/ecommerce-admin/', // 👈 VERY IMPORTANT
+  plugins: [react()],
+})
