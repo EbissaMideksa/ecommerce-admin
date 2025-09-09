e8a7c819701b111dc2e1792a8b30197e77f1adca
EebbaaM <neweebbiwaan1@gmail.com>
2025-09-09 15:45:08 +0300

    Set correct base path in vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   base: '/ecommerce-admin/',
   plugins: [react()],
   server: {
     proxy: {
